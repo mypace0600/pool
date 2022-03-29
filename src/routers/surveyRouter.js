6ecbe216b9d259482d47c6f8e15f266dcd3ac23b
mypace0600 <mypace0600@gmail.com>
2022-03-30 01:00:58 +0900

    photo5

--- src/routers/surveyRouter.js
+++ src/routers/surveyRouter.js
@@ -13,6 +13,9 @@ import surveyPhoto from "../middlewares";
 const surveyRouter = express.Router();
 surveyRouter.route("/:id([0-9a-f]{24})/check").get(getSurveyCheck).post(postSurveyCheck);
 surveyRouter.route("/:id([0-9a-f]{24})/delete").get(getDelete);
-surveyRouter.route("/upload").get(getUpload).post(surveyPhoto.single("photo"),postUpload);
+surveyRouter
+    .route("/upload")
+    .get(getUpload)
+    .post(surveyPhoto.single("photo"), postUpload);
 
 export default surveyRouter;
